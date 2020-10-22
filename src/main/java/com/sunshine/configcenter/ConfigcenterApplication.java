eed96ad50789cd0cf2bf1cc872904bdaf236846b
wkj007 <m15692404703@163.com>
2020-10-23 00:14:39 +0800

    Initial commit

---
+++ src/main/java/com/sunshine/configcenter/ConfigcenterApplication.java
@@ -0,0 +1,19 @@
+package com.sunshine.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunshine/configcenter/ConfigcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshine.configcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
